632a2e21604182bbf2b6de6dbbf38abb51e6db9d
Ryan Njoroge <keddelyronjoz@gmail.com>
2021-07-08 06:49:10 +0300

    fix: Added vscode/settings to .gitignore

---
+++ lib/configs/secrets/secrets.dart
@@ -0,0 +1,4 @@
+const String clientIdApi = 'Gqukzo2rcAV3tkbeU7KXOqvwHNrAOAoRb2iHtwCy';
+const String clientSecretApi =
+    'XspdXAXruWqxg2GfQQwaP4JogB7BrpkRJP0e1lcKdwhs9fkbQEtTn18NbjvjX0IDWXoan3hqoORT4OYnMXNJHjdjU89OqATPXZof5tfzkPSz04g4ECbcedCoQBJdePg2';
+const String baseUrl = 'http://127.0.0.1:8000';
